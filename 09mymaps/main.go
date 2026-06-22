a826a582c2d956185cf212329b4d9d85436ce045
bhumika019579 <bhumikachanchlani014@gmail.com>
2026-06-22 23:55:27 +0530

    more about map

--- 09mymaps/main.go
+++ 09mymaps/main.go
@@ -8,5 +8,12 @@ func main() {
 	languages["RB"] = "ruby"
 	languages["PY"] = "python"
 	fmt.Println("list of all languages:",languages)
+	fmt.Println("JS shorts for :",languages["JS"])
+	delete(languages,"RB")
+	fmt.Println("list for all languages:",languages)
+	//loops are interesting in golang
+	for key,value := range languages{
+		fmt.Printf("for key %v,value is %v\n",key,value)
+	}
 
 }
