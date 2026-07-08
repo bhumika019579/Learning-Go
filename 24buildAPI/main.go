8605c718416ea0e9992d995b2986fa91e722f159
bhumika019579 <bhumikachanchlani014@gmail.com>
2026-07-08 19:52:13 +0530

    building API

---
+++ 24buildAPI/main.go
@@ -0,0 +1,23 @@
+package main
+
+// model for courses-file
+type Course struct{
+	CourseId string `json:"courseid"`
+	CourseName string `json:"coursename"`
+	CoursePrice int `json:"price"`
+	Author *Author `json:"author"`
+
+}
+type Author struct{
+	Fullname string `json:"fullname"`
+	Website string `json:"website"`
+}
+//fake db
+var courses []Course
+// middleware,helper-file
+func(c*Course) IsEmpty() bool{
+return c.CourseId=="" && c.CourseName==""
+}
+func main(){
+
+}
