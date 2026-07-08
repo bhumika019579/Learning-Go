1f3125c25e3c21cbf4f76b6a76035be6a9f32c02
bhumika019579 <bhumikachanchlani014@gmail.com>
2026-07-08 21:01:21 +0530

    add func to get one course

--- 24buildAPI/main.go
+++ 24buildAPI/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // model for courses-file
@@ -29,11 +31,27 @@ func main(){
 }
 // controllers-file 
 //serve home route
-func serveHome(w http.ResponseWriter,r *http.Request){
+func serveHome(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("<h1>welcome to API by learncode online</h1>"))
 }
-func getAllCourses(w http.ResponseWriter,r *http.Request){
+func getAllCourses(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Get all courses ")
 	w.Header().Set("content-type","application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+func getOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Printf("Get one course")
+	w.Header().Set("content-type","application/json")
+	//grab id from request
+	params:=mux.Vars(r)
+    //loop through courses,find matching id and return the response
+	for _,Course:=range courses{
+		if Course.CourseId==params["id"]{
+			json.NewEncoder(w).Encode(Course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("no course found with given id ")
+	return 
+
 }
