73f77bb6a084a0e1e96cfe2d6809037aee9fd460
bhumika019579 <bhumikachanchlani014@gmail.com>
2026-07-13 17:23:20 +0530

    fixing creating book route

--- 25go-fibre-postgres/main.go
+++ 25go-fibre-postgres/main.go
@@ -1,15 +1,30 @@
 package main
-import(
+
+import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
-
 )
-
+type Book struct{
+	Author string `json:"author"`
+	Title string   `json-"title"`
+	Publisher string `json-"publisher"`
+}
 
 
 type Repository struct{
 	DB*gorm.DB
+}
+func(r*Repository) CreateBook(context*fiber.Ctx)error{
+	book:=Book{}
+	err:=context.BodyParser(&book)
+	if err!=nil{
+		context.Status(http.StatisUnprocessableEntity).JSON(
+		&fiber.Map{"message":"request failed"})
+		return err
+	}
 }
 // a struct method not a regular function
 func(r*Repository)SetupRoutes(app *fiber.App){
