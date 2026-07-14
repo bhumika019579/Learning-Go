de77710618a804f45a7859ac9741871a5b07bc75
bhumika019579 <bhumikachanchlani014@gmail.com>
2026-07-14 20:56:19 +0530

    modified postgres.go

--- 25go-fibre-postgres/storage/postgres.go
+++ 25go-fibre-postgres/storage/postgres.go
@@ -1,21 +1,32 @@
 package storage
 
+import (
+	"fmt"
 
-import(
-	""
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 
 type config struct{
 	Host    string
 	Port     string
-	password   string
-	user     string
+	Password   string
+	User     string
 	DBName    string
 	SSLMode    string
 }
 
 
 func NewConnection(config *config)(*gorm.DB,error){
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.Host,config.Port,config.User,config.Password,config.DBName,config.SSLMode,
+	)
+	db,err:=gorm.Open(postgres.Open(dsn),&gorm.Config{})
+	if err!=nil{
+		return db,err
+	}
+	return db,nil
 
 }
